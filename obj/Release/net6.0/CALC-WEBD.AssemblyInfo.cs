56741c6a7dd435d4c452e6db2f58a1c603cf3158
Reno594 <jdieppacastillo@gmail.com>
2024-12-24 19:28:57 +0100

    Eliminar archivos duplicados y corregir configuración

--- obj/Release/net6.0/CALC-WEBD.AssemblyInfo.cs
+++ obj/Release/net6.0/CALC-WEBD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CALC-WEBD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb7c0f35456a1a413ed936ebecbc3bedcd2719d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a299524e06149689bf61a435cb58b1ab4f14ccd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CALC-WEBD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CALC-WEBD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
